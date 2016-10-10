202c8ba2f8660b0b39dca44de9a04f6995ec3cc6
Nanda <nandakumarpurohit@gmail.com>
2016-10-09 20:32:42 -0400

    Initial files

---
+++ dev/js/index.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from "react-dom";
+import {Provider} from 'react-redux';
+
+
+ReactDOM.render(
+    <h1>Hello there...</h1>,
+    document.getElementById('root')
+);
